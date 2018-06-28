b4dbce53fbe8ae1a346197b0e69fe35ac1b8dec4
umpenrage <mark.umpenhour@gmail.com>
2018-06-28 16:06:32 -0400

    Update chapter8challenge4 monkey header file.cpp

--- chapter8challenge4 monkey header file.cpp
+++ chapter8challenge4 monkey header file.cpp
@@ -2,6 +2,7 @@
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
+ * make sure to change the extension from .cpp to .h
  */
 
 /* 
